9ff6907e25bfbbc34e2511bbf44ffcff99107480
Harshit Lakhawat <110206harshit@gmail.com>
2025-01-06 19:19:16 +0530

    Update and rename oops constructor.cpp to oops constructor and this .cpp

--- oops constructor and this .cpp
+++ oops constructor and this .cpp
@@ -46,3 +46,46 @@ int main() {
     
     return 0;
 }
+
+
+
+// this function
+#include <iostream>
+#include <string>
+using namespace std;
+
+class teacher {
+    private:
+        double salary;   // now salary is private
+    public:
+        string name;
+        string subject;  // Use 'subjects' here
+        string dept;
+        teacher(){
+            dept = "CS";
+        }
+        // Parameterized constructor
+        teacher(string name, string dept, string subject, double salary) {
+            this -> name = name;
+            this -> dept = dept;
+            this -> subject = subject;   // Assign to 'subjects' here
+            this -> salary = salary;
+        }
+
+    // Methods or member functions
+    void changedept(string newdept) {
+        dept = newdept;
+    }
+
+    void getinfo() {
+        cout << "Name: " << name << endl;
+        cout << "Subject: " << subject << endl;  // Print 'subjects' here
+    }
+};
+
+int main() {
+    teacher t1("harshit", "CS", "C++", 250000); 
+    t1.getinfo();
+    
+    return 0;
+}
