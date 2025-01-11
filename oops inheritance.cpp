deafe89cf144bec37253859e14b8ec8e61848df3
Harshit Lakhawat <110206harshit@gmail.com>
2025-01-11 11:46:45 +0530

    Update oops inheritance.cpp

--- oops inheritance.cpp
+++ oops inheritance.cpp
@@ -37,3 +37,43 @@ int main(){
     
     return 0;
 }
+
+
+// inheriatance with parameterized constructor 
+//inheritance : studnet me name aur age nahi h phir bhi errorni aaya kyuki person ki properties inherite karli student class ne
+
+#include <iostream>
+#include <string>  
+using namespace std;
+
+class person{
+    public:
+        string name;
+        int age;
+        
+        person(string name, int age){
+            this -> name = name;
+            this -> age = age;
+        }
+};
+class student:public person {
+    public:
+         int rollno;
+         student(string name, int age, int rollno) : person(name, age){
+             this -> rollno = rollno;
+         }
+         
+         void getinfo(){
+             cout << "name : " << name << endl;
+             cout << "age : " << age << endl;
+             cout << "rollno : " << rollno << endl;
+             
+         }
+};
+int main(){
+    student s1("harshit", 12 , 7);
+
+    s1.getinfo();
+    
+    return 0;
+}
